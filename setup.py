4c594801d654711f9beca28b7eb4e1930b6c7868
Priyatham Sai Chand <priyathamsaichand@gmail.com>
2021-09-07 07:28:09 +0530

    cookie cutter plain

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='translate english to german or vice versa using transformers networks for machine language translation.',
+    author='B. Priyatham Sai Chand',
+    license='MIT',
+)
